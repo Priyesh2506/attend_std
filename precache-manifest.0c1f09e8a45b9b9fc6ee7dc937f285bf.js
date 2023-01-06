ce5c9134a97f2b1bade1a1d3c881d11c45f6e9ec
Priyesh Pandey <priyeshpandey559@gmail.com>
2023-01-06 23:24:55 +0530

    Updates

---
+++ precache-manifest.0c1f09e8a45b9b9fc6ee7dc937f285bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0125a23634a7f6c5ad8a",
+    "url": "/attend_std/static/css/main.69a2c46b.chunk.css"
+  },
+  {
+    "revision": "0125a23634a7f6c5ad8a",
+    "url": "/attend_std/static/js/main.bbaf8d5c.chunk.js"
+  },
+  {
+    "revision": "dba8451b5a1b0220b4d3",
+    "url": "/attend_std/static/js/runtime~main.a1f4e821.js"
+  },
+  {
+    "revision": "de049151709b77819ca5",
+    "url": "/attend_std/static/js/2.5f18d180.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/attend_std/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "722185d71d64308bcafa0766735c64b5",
+    "url": "/attend_std/index.html"
+  }
+];
